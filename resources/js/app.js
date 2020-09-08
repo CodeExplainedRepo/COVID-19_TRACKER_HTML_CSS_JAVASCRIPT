bdfc176f403d0425191fb80f3bf802d7a457fe19
CodeExplainedRepo <41912954+CodeExplainedRepo@users.noreply.github.com>
2020-09-08 13:02:44 +0100

    API updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,132 +16,188 @@ const ctx = document.getElementById("axes_line_chart").getContext("2d");
 
 // APP VARIABLES
 let app_data = [],
-	cases_list = [],
-	recovered_list = [],
-	deaths_list = [],
-	deaths = [],
-	formatedDates = [];
+  cases_list = [],
+  recovered_list = [],
+  deaths_list = [],
+  deaths = [],
+  formatedDates = [];
 
 // GET USERS COUNTRY CODE
 let country_code = geoplugin_countryCode();
 let user_country;
-country_list.forEach( country => {
-	if( country.code == country_code ){
-		user_country = country.name;
-	}
+country_list.forEach((country) => {
+  if (country.code == country_code) {
+    user_country = country.name;
+  }
 });
 
 /* ---------------------------------------------- */
-/*                API URL AND KEY                 */
+/*                     FETCH API                  */
 /* ---------------------------------------------- */
+function fetchData(country) {
+  user_country = country;
+  console.log(country);
+  country_name_element.innerHTML = "Loading...";
 
-function fetchData(user_country){
-	country_name_element.innerHTML = "Loading...";
+  (cases_list = []),
+    (recovered_list = []),
+    (deaths_list = []),
+    (dates = []),
+    (formatedDates = []);
 
-	cases_list = [], recovered_list =[], deaths_list = [], dates = [], formatedDates = [];
-	
-	fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=${user_country}`, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
-			"x-rapidapi-key": "7e269ec140msh8a5df9cfc21b4b4p1c1e3ejsn9aba26afc6e0"
-		}
-	})
-	.then( response => {
-		return response.json();
-	})
-	.then( data => {
-		dates = Object.keys(data);
-		
-		dates.forEach( date => {
-			let DATA = data[date];
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
-			formatedDates.push(formatDate(date));
-			app_data.push(DATA);
-			cases_list.push(parseInt(DATA.total_cases.replace(/,/g, "")));
-			recovered_list.push(parseInt(DATA.total_recovered.replace(/,/g, "")));
-			deaths_list.push(parseInt(DATA.total_deaths.replace(/,/g, "")));
-		})
-	})
-	.then( () => {
-		updateUI();
-	})
-	.catch( error => {
-		alert(error);
-	})
+  const api_fetch = async (country) => {
+    await fetch(
+      "https://api.covid19api.com/total/country/" +
+        country +
+        "/status/confirmed",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          dates.push(entry.Date);
+          cases_list.push(entry.Cases);
+        });
+      });
+
+    await fetch(
+      "https://api.covid19api.com/total/country/" +
+        country +
+        "/status/recovered",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          recovered_list.push(entry.Cases);
+        });
+      });
+
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/deaths",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          deaths_list.push(entry.Cases);
+        });
+      });
+
+    updateUI();
+  };
+
+  api_fetch(country);
 }
 
 fetchData(user_country);
 
 // UPDATE UI FUNCTION
-function updateUI(){
-	updateStats();
-	axesLinearChart();
+function updateUI() {
+  updateStats();
+  axesLinearChart();
 }
 
-function updateStats(){
-	let last_entry = app_data[app_data.length - 1];
-	let before_last_entry = app_data[app_data.length - 2];
+function updateStats() {
+  const total_cases = cases_list[cases_list.length - 1];
+  const new_confirmed_cases = total_cases - cases_list[cases_list.length - 2];
 
-	country_name_element.innerHTML = last_entry.country_name;
+  const total_recovered = recovered_list[recovered_list.length - 1];
+  const new_recovered_cases =
+    total_recovered - recovered_list[recovered_list.length - 2];
 
-	total_cases_element.innerHTML = last_entry.total_cases || 0;
-	new_cases_element.innerHTML = `+${last_entry.new_cases || 0 }`;
+  const total_deaths = deaths_list[deaths_list.length - 1];
+  const new_deaths_cases = total_deaths - deaths_list[deaths_list.length - 2];
 
-	recovered_element.innerHTML = last_entry.total_recovered || 0;
-	new_recovered_element.innerHTML = `+${parseInt(last_entry.total_recovered.replace(/,/g, "")) - parseInt(before_last_entry.total_recovered.replace(/,/g, ""))}`;
-	
-	deaths_element.innerHTML = last_entry.total_deaths;
-	new_deaths_element.innerHTML = `+${last_entry.new_deaths || 0}`;
+  country_name_element.innerHTML = user_country;
+  total_cases_element.innerHTML = total_cases;
+  new_cases_element.innerHTML = +${new_confirmed_cases};
+  recovered_element.innerHTML = total_recovered;
+  new_recovered_element.innerHTML = +${new_recovered_cases};
+  deaths_element.innerHTML = total_deaths;
+  new_deaths_element.innerHTML = +${new_deaths_cases};
+
+  // format dates
+  dates.forEach((date) => {
+    formatedDates.push(formatDate(date));
+  });
+
+  console.log(dates);
 }
 
 // UPDATE CHART
 let my_chart;
-function axesLinearChart(){
+function axesLinearChart() {
+  if (my_chart) {
+    my_chart.destroy();
+  }
 
-	if(my_chart){
-		my_chart.destroy();
-	}
-
-	my_chart = new Chart(ctx, {
-		type: 'line',
-		data: {
-			datasets: [{
-				label: 'Cases',
-				data: cases_list,
-				fill : false,
-				borderColor : '#FFF',
-				backgroundColor: '#FFF',
-				borderWidth : 1
-			},{
-				label: 'Recovered',
-				data: recovered_list,
-				fill : false,
-				borderColor : '#009688',
-				backgroundColor: '#009688',
-				borderWidth : 1
-			},{
-				label: 'Deaths',
-				data: deaths_list,
-				fill : false,
-				borderColor : '#f44336',
-				backgroundColor: '#f44336',
-				borderWidth : 1
-			}],
-			labels: formatedDates
-		},
-		options: {
-			responsive : true,
-			maintainAspectRatio : false
-		}
-	});
+  my_chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "Cases",
+          data: cases_list,
+          fill: false,
+          borderColor: "#FFF",
+          backgroundColor: "#FFF",
+          borderWidth: 1,
+        },
+        {
+          label: "Recovered",
+          data: recovered_list,
+          fill: false,
+          borderColor: "#009688",
+          backgroundColor: "#009688",
+          borderWidth: 1,
+        },
+        {
+          label: "Deaths",
+          data: deaths_list,
+          fill: false,
+          borderColor: "#f44336",
+          backgroundColor: "#f44336",
+          borderWidth: 1,
+        },
+      ],
+      labels: formatedDates,
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
 }
 
 // FORMAT DATES
-const monthsNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const monthsNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
-function formatDate(dateString){
-	let date = new Date(dateString);
+function formatDate(dateString) {
+  let date = new Date(dateString);
 
-	return `${date.getDate()} ${monthsNames[date.getMonth()]}`;
+  return ${date.getDate()} ${monthsNames[date.getMonth() - 1]};
 }
